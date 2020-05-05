83d1abb901154e5848f9fb7ffe8fd75cf68f7207
Ben-1327 <ff.walker1327@gmail.com>
2020-05-05 16:08:17 +0900

    200505 1600

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :books do
     resource :book_comments, only: [:create,:destroy]
+    resource :favorites, only: [:create,:destroy]
   end
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update, :destroy,:create]
